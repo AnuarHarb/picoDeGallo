0f2f256f06760663a3bcd33273683149b75799eb
Anuar Harb <anuarharb@hotmail.com>
2019-04-22 17:56:39 -0500

    build

---
+++ build/precache-manifest.5a017405a15915656c10f8048f012462.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a259c86de742dde801f7301695b1e67e",
+    "url": "/static/media/los-mas-salsa.a259c86d.png"
+  },
+  {
+    "revision": "2f4789b27de04edef4912d36eeda25d4",
+    "url": "/static/media/hugo.2f4789b2.png"
+  },
+  {
+    "revision": "97b3cd51b03905326276c450efe5c412",
+    "url": "/static/media/galloway-regular.97b3cd51.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d48c9f171fbdd2fad271",
+    "url": "/static/js/main.3bfb2d4c.chunk.js"
+  },
+  {
+    "revision": "420412071e85f39972b1",
+    "url": "/static/js/2.df253bec.chunk.js"
+  },
+  {
+    "revision": "d48c9f171fbdd2fad271",
+    "url": "/static/css/main.97e62ab4.chunk.css"
+  },
+  {
+    "revision": "ffdc0e6f144eedb8bc712e7b085d0986",
+    "url": "/index.html"
+  }
+];
